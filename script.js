e9694975324a6bcb88ab7eabedd438944841d071
BraveSentry <quilbert@gmx.de>
2021-01-11 18:42:25 +0100

    let users customize the grid

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
 const container = document.querySelector(`#container`);
 
-let pixelsX = 4;
 let pixelsY = 4;
+let pixelsX = 4;
 
-let dimensions = String(100 / pixels) + "px";
+gridMaker(pixelsY, pixelsX);
 
-for (i = 1; i <= pixelsX; i++) {
-  for (j = 1; j <= pixelsY; j++) {
-    const div1 = document.createElement('div');
+function gridMaker(pixelsY, pixelsX) {
 
-    div1.classList.add(`grid-item`);
+  let dimensions = String(100 / pixelsY) + "px";
 
-    div1.style.width = "10px";
-    div1.style.height = "10px";
+  for (let i = 1; i <= pixelsY; i++) {
+    for (let j = 1; j <= pixelsX; j++) {
+      const div1 = document.createElement('div');
 
-    div1.style.gridArea = `${i} / ${j}`;
+      div1.classList.add(`grid-item`);
 
-    div1.addEventListener("mouseover", function(){
-        div1.classList.add(`hovered`);
-    });
+      div1.style.width = "10px";
+      div1.style.height = "10px";
 
-    container.appendChild(div1);
+      div1.style.gridArea = `${i} / ${j}`;
+
+      div1.addEventListener("mouseover", function(){
+          div1.classList.add(`hovered`);
+      });
+      console.log("happns")
+
+      container.appendChild(div1);
+    };
   };
 };
 
-let button = document.querySelector(`#resetGrid`);
+let resetButton = document.querySelector(`#resetGrid`);
+let newGridButton = document.querySelector(`#newGrid`);
 
-let divs = document.querySelectorAll(`div`);
 
-button.addEventListener(`click`, function(){
+newGridButton.addEventListener(`click`, function(){
+  while (container.hasChildNodes()) {
+    container.removeChild(container.childNodes[0]);
+  };
+  let numX = prompt("Please enter the width of your grid:", 4);
+  while (numChecker(numX) === false) {
+    numX = prompt("Please enter a number between 2 and 99.");
+  }
+  let numY = prompt("Please enter the width of your grid:", 4);
+  while (numChecker(numY) === false) {
+    numY = prompt("Please enter a number between 2 and 99.");
+  }
+  pixelsX = numX;
+  pixelsY = numY;
+  gridMaker(pixelsX, pixelsY);
+});
+
+function numChecker(num) {
+  if (num > 99 || (num < 2) || isNaN(num)) {
+    return false;
+  }
+  else {
+    return true;
+  };
+};
+
+function resetGrid() {
+  let divs = document.querySelectorAll(`.grid-item`);
   for (let k = 0; k < divs.length; k++) {
     divs[k].classList.remove(`hovered`);
   }
+};
+
+resetButton.addEventListener(`click`, function(){
+  resetGrid();
 });
